3abbf762c153b3e2937cfb724cc0100eced5aac8
BM00786 <biswa.bm60@gmail.com>
2022-04-10 21:33:03 +0530

    https://github.com/BiswadeepMondal/React-Web.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-
+import clevertap from 'clevertap-web-sdk';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <header className="App-header">
+       <h1>CLEVERTAP REACT PROJECT</h1>
+       <div>
+       <button className="btn" onClick={createUser}>SET IDENTITY</button>
+       <button className="btn" onClick={showid}>SHOW CT ID</button>
+       <button className="btn" onClick={notify}>ASK PERMISSION</button>
+       <button className="btn" onClick={pushevent}>PUSH EVENT</button>
+       </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 id="test"> </h2>
+       
       </header>
     </div>
   );
 }
+
+function createUser() {
+ 
+  clevertap.onUserLogin.push({
+   "Site": {
+     "Name": "Biswa react",            // String
+     "Identity": 5447,              // String or number
+     "Email": "biswareact@web.com",         // Email address of the user
+     "Phone": "+91779299",           // Phone (with the country code)
+     "Gender": "M",                     // Can be either M or F
+     "DOB": new Date(),                 // Date of Birth. Date object
+  // optional fields. controls whether the user will be sent email, push etc.
+     "MSG-email": true,                // Disable email notifications
+     "MSG-push": true,                  // Enable push notifications
+     "MSG-sms": true,                   // Enable sms notifications
+     "MSG-whatsapp": true,              // Enable WhatsApp notifications
+   
+    }
+  })
+  console.log("user created");
+}
+
+function showid(){
+	 var CT = clevertap.getCleverTapID();
+   alert(CT);
+  //document.getElementById("test").innerHTML = CT;
+  console.log(CT+"ID");
+  }
+
+  function notify(){
+    
+    clevertap.notifications.push({
+        "titleText":'Would you like to receive Push Notifications?',
+        "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
+        "okButtonText":'Sign me up!',
+        "rejectButtonText":'No thanks',
+        "okButtonColor":'#778046'});
+        console.log("permission asked");
+    }
+
+    function pushevent(){
+      clevertap.event.push("biswaWebpush");
+      console.log("event pushed");
+      }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import clevertap from 'clevertap-web-sdk';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-analytics.js";
+clevertap.init('TEST-4W5-9RR-646Z'); 
+clevertap.privacy.push({optOut: false}); //set the flag to true, if the user of the device opts out of sharing their data
+clevertap.privacy.push({useIP: true}); //set the flag to true, if the user agrees to share their IP data
+		
+			// TODO: Add SDKs for Firebase products that you want to use
+			// https://firebase.google.com/docs/web/setup#available-libraries
+		  
+			// Your web app's Firebase configuration
+			// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+			const firebaseConfig = {
+			  apiKey: "AIzaSyA_Ogl_uvT3_b9isNPxr1QtwVYJ-CL0E3s",
+			  authDomain: "ctest2-4f342.firebaseapp.com",
+			  projectId: "ctest2-4f342",
+			  storageBucket: "ctest2-4f342.appspot.com",
+			  messagingSenderId: "68868130286",
+			  appId: "1:68868130286:web:13f41cf53427ffbb84faff",
+			  measurementId: "G-FGG4RYW3JL"
+			};
+		  
+			// Initialize Firebase
+			const app = initializeApp(firebaseConfig);
+			const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+ 
   document.getElementById('root')
 );
 
